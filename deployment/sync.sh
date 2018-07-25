21078f73329b3b13d5f37ac6d07c3384c066d276
Jozef Hartinger <jharting@redhat.com>
2018-07-25 08:51:28 +0200

    add OpenShift configs

---
+++ deployment/sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+if [[ $(oc project -q) != remediations* ]]; then
+    echo "unexpected project: $(oc project -q)"
+    exit 1;
+fi
+
+for i in *.yaml; do
+    echo "Updating $i"
+    oc process -f $i --local --ignore-unknown-parameters NAMESPACE="$(oc project --short)" | oc apply -f -
+done;
