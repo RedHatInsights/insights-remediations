f320471484a9bcbcf5452eae6a94e34d7f9920cc
Jozef Hartinger <jharting@redhat.com>
2018-08-07 08:25:59 +0200

    request - minor improvements

--- src/util/request.js
+++ src/util/request.js
@@ -1,18 +1,24 @@
 'use strict';
 
 const config = require('../config');
-const request = require('request-promise').defaults({
+
+const base = require('request-promise').defaults({
     timeout: config.requestTimeout
 });
+
+function wrap (request) {
+    const wrapper = (uri, options) => request(uri, options).catch(e => {
+        e.options = (typeof uri === 'object') ? uri : options;
+        throw e;
+    });
+
+    wrapper.unwrap = () => request;
+    wrapper.defaults = opts => wrap(request.defaults(opts));
+
+    return wrapper;
+}
 
 /*
  * Wraps request to provide better debugging (include request opts with the error object)
  */
-module.exports = function (uri, options) {
-    return request(uri, options).catch(e => {
-        e.options = (typeof uri === 'object') ? uri : options;
-        throw e;
-    });
-};
-
-module.exports.unwrap = () => request;
+module.exports = wrap(base);
--- src/util/request.unit.js
+++ src/util/request.unit.js
@@ -5,7 +5,7 @@ const request = require('./request');
 
 test('includes request options in the error object', async function () {
     const options = {
-        url: `http://localhost:9003`,
+        url: `http://localhost:9003/health`,
         timeout: 1
     };
 
@@ -15,6 +15,26 @@ test('includes request options in the error object', async function () {
     } catch (e) {
         // the exact error code varies between these two values randomly
         // https://github.com/request/request/issues/2320
+        e.should.have.property('error');
+        ['ESOCKETTIMEDOUT', 'ETIMEDOUT'].includes(e.error.code).should.be.true();
+        e.should.have.property('options', options);
+    }
+});
+
+test('includes request options in the error object of a request child', async function () {
+    const options = {
+        url: `http://localhost:9003/health`
+    };
+
+    const req = request.defaults({ timeout: 1 });
+
+    try {
+        await req(options);
+        throw new Error('not thrown!');
+    } catch (e) {
+        // the exact error code varies between these two values randomly
+        // https://github.com/request/request/issues/2320
+        e.should.have.property('error');
         ['ESOCKETTIMEDOUT', 'ETIMEDOUT'].includes(e.error.code).should.be.true();
         e.should.have.property('options', options);
     }
