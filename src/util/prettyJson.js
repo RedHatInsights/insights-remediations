4f9da1fb79832da73e472255277d41c55fb61ada
Jozef Hartinger <jharting@redhat.com>
2018-08-20 09:08:26 +0200

    add pretty JSON support

---
+++ src/util/prettyJson.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const PARAM = 'pretty';
+const SPACES = 4;
+
+module.exports = function (req, res, next) {
+    if (typeof req.query[PARAM] !== 'undefined' ||
+        (typeof req.headers['user-agent'] === 'string' && req.headers['user-agent'].startsWith('Mozilla'))) {
+
+        res.json = function (body) {
+            if (!res.get('Content-Type')) {
+                res.set('Content-Type', 'application/json');
+            }
+
+            return res.send(JSON.stringify(body, null, SPACES));
+        };
+    }
+
+    next();
+};
