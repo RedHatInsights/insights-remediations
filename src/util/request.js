919fea61e7973159bcbab8865e82f6d660f033d8
Jozef Hartinger <jharting@redhat.com>
2018-08-20 09:25:42 +0200

    log outbound HTTP requests

--- src/util/request.js
+++ src/util/request.js
@@ -1,15 +1,20 @@
 'use strict';
 
 const config = require('../config');
+const log = require('./log');
+
 const base = require('request-promise').defaults({
     timeout: config.requestTimeout
 });
 
 function wrap (request) {
-    const wrapper = options => request(options).catch(e => {
-        e.options = options;
-        throw e;
-    });
+    const wrapper = options => {
+        log.debug({uri: options.uri, method: options.method}, 'outbound HTTP request');
+        return request(options).catch(e => {
+            e.options = options;
+            throw e;
+        });
+    };
 
     wrapper.unwrap = () => request;
     wrapper.defaults = opts => wrap(request.defaults(opts));
