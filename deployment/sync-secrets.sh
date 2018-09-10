7711ce25006a021f1662c081bbe06e9785f5f38e
Jozef Hartinger <jharting@redhat.com>
2018-09-10 16:01:41 +0200

    fix sync-secret script

--- deployment/sync-secrets.sh
+++ deployment/sync-secrets.sh
@@ -9,5 +9,5 @@ fi
 
 for i in secrets/*.yaml; do
     echo "Updating $i"
-    oc process -f $i --local --ignore-unknown-parameters NAMESPACE="$(oc project --short)" REDIS_PASS=${REDIS_PASS} CONTENT_SERVER_TOKEN=${CONTENT_SERVER_TOKEN} DB_PASS=${DB_PASS} | oc apply -f -
+    oc process -f $i --local --ignore-unknown-parameters NAMESPACE="$(oc project --short)" REDIS_PASS="${REDIS_PASS}" CONTENT_SERVER_TOKEN="${CONTENT_SERVER_TOKEN}" DB_PASS="${DB_PASS}" | oc apply -f -
 done;
