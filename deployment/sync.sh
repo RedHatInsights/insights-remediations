e590781c97f71829b4536e9a611fd4d2733dc01f
Jozef Hartinger <jharting@redhat.com>
2018-11-03 18:31:06 +0100

    deployment configuration changes

--- deployment/sync.sh
+++ deployment/sync.sh
@@ -2,12 +2,19 @@
 
 set -e
 
-if [[ $(oc project -q) != remediations* || $(oc project -q) == "remediations-pr" ]]; then
+NAMESPACE=$(oc project -q)
+
+if [[ $NAMESPACE != remediations* || $NAMESPACE == "remediations-pr" ]]; then
     echo "unexpected project: $(oc project -q)"
     exit 1;
 fi
 
+HOSTNAME="${NAMESPACE}.5a9f.insights-dev.openshiftapps.com"
+if [[ $NAMESPACE == "remediations-prod" ]]; then
+    HOSTNAME="remediations.1b13.insights.openshiftapps.com"
+fi
+
 for i in configuration/*.yaml; do
     echo "Updating $i"
-    oc process -f $i --local --ignore-unknown-parameters NAMESPACE="$(oc project --short)" | oc apply -f -
+    oc process -f $i --local --ignore-unknown-parameters NAMESPACE="$(oc project --short)" HOSTNAME="$HOSTNAME" | oc apply -f -
 done;
