443259a54b2d2af565fcfb3f7902992ca547edaa
Jozef Hartinger <jharting@redhat.com>
2018-11-21 14:53:08 +0100

    update seeders

---
+++ db/seeders/20180823074644-tests01.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const tenant = 'test';
+const owner = 100;
+
+const opts = {
+    returning: true
+};
+
+exports.up = async q => {
+    const remediations = await q.bulkInsert('remediations', [{
+        id: '66eec356-dd06-4c72-a3b6-ef27d1508a02',
+        name: 'remediation 1',
+        tenant,
+        owner,
+        created_at: '2018-10-04T08:19:36.641Z',
+        updated_at: '2018-10-04T08:19:36.641Z'
+    }, {
+        id: 'cbc782e4-e8ae-4807-82ab-505387981d2e',
+        name: 'remediation 2',
+        tenant,
+        owner,
+        created_at: '2018-10-04T08:19:36.641Z',
+        updated_at: '2018-10-04T08:19:36.641Z'
+    }, {
+        id: 'e809526c-56f5-4cd8-a809-93328436ea23',
+        name: null,
+        tenant,
+        owner,
+        created_at: '2018-10-04T08:19:36.641Z',
+        updated_at: '2018-10-04T08:19:36.641Z'
+    }, {
+        id: 'e67118cc-28ec-4b55-afe9-2b5cfab24f13',
+        name: 'to be deleted',
+        tenant,
+        owner,
+        created_at: '2018-10-04T08:19:36.641Z',
+        updated_at: '2018-10-04T08:19:36.641Z'
+    }], opts);
+
+    const systems = await q.bulkInsert('systems', [{
+        id: 'fc94beb8-21ee-403d-99b1-949ef7adb762'
+    }, {
+        id: '1f12bdfc-8267-492d-a930-92f498fe65b9'
+    }], opts);
+
+    const issues = await q.bulkInsert('remediation_issues', [{
+        remediation_id: remediations[0].id,
+        issue_id: 'vulnerabilities:CVE_2017_6074_kernel|KERNEL_CVE_2017_6074'
+    }, {
+        remediation_id: remediations[0].id,
+        issue_id: 'vulnerabilities:CVE-2017-17713'
+    }, {
+        remediation_id: remediations[0].id,
+        issue_id: 'vulnerabilities:RHSA-2018:0502'
+    }, {
+        remediation_id: remediations[0].id,
+        issue_id: 'advisor:bond_config_issue|BOND_CONFIG_ISSUE'
+    }, {
+        remediation_id: remediations[0].id,
+        issue_id: 'compliance:sshd_disable_root_login'
+    }], opts);
+
+    await q.bulkInsert('remediation_issue_systems', [{
+        system_id: systems[0].id,
+        remediation_issue_id: issues[0].id
+    }, {
+        system_id: systems[0].id,
+        remediation_issue_id: issues[1].id
+    }, {
+        system_id: systems[0].id,
+        remediation_issue_id: issues[2].id
+    }, {
+        system_id: systems[0].id,
+        remediation_issue_id: issues[3].id
+    }, {
+        system_id: systems[0].id,
+        remediation_issue_id: issues[4].id
+    }, {
+        system_id: systems[1].id,
+        remediation_issue_id: issues[0].id
+    }]);
+};
---
+++ db/seeders/20181121125041-demo.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const tenant = 'demo';
+const owner = 1;
+
+const opts = {
+    returning: true
+};
+
+exports.up = async q => {
+    const remediations = await q.bulkInsert('remediations', [{
+        id: '9939e04a-a936-482d-a317-008c058f7918',
+        name: 'Patch vulnerabilities on production systems',
+        tenant,
+        owner,
+        created_at: '2018-11-21T10:19:38.541Z',
+        updated_at: '2018-11-21T10:19:38.541Z'
+    }, {
+        id: '0bcebc81-0d53-4f77-b0f0-1a56e01a55fd',
+        name: 'Recommended configuration changes',
+        tenant,
+        owner,
+        created_at: '2018-11-21T09:19:38.541Z',
+        updated_at: '2018-11-21T09:19:38.541Z'
+    }], opts);
+
+    const systems = await q.bulkInsert('systems', [{
+        id: '8dadd8d7-5f1d-49c3-a560-af3cada7ce83'
+    }, {
+        id: 'fc84c991-a029-4882-bc9c-7e351a73b59f'
+    }, {
+        id: '58b2837a-5df5-4466-9033-c4b46248d4b4'
+    }, {
+        id: '29dafba0-c190-4acd-998d-074ba0aee477'
+    }], opts);
+
+    const issues = await q.bulkInsert('remediation_issues', [{
+        remediation_id: remediations[0].id,
+        issue_id: 'vulnerabilities:CVE_2017_6074_kernel|KERNEL_CVE_2017_6074'
+    }, {
+        remediation_id: remediations[0].id,
+        issue_id: 'vulnerabilities:CVE-2017-17713'
+    }, {
+        remediation_id: remediations[0].id,
+        issue_id: 'vulnerabilities:RHSA-2018:0502'
+    }, {
+        remediation_id: remediations[1].id,
+        issue_id: 'advisor:bond_config_issue|BOND_CONFIG_ISSUE'
+    }, {
+        remediation_id: remediations[1].id,
+        issue_id: 'compliance:sshd_disable_root_login'
+    }], opts);
+
+    issues.forEach(issue => {
+        const systemSlice = (issue.remediation_id === remediations[0].id) ? systems.slice(0, 2) : systems.slice(2);
+
+        q.bulkInsert('remediation_issue_systems', systemSlice.map(system => ({
+            remediation_issue_id: issue.id,
+            system_id: system.id
+        })));
+    });
+};
