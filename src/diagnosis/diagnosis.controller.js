7bcf9b36326389e4d55b58b22f5534b7acd6a6b8
Jozef Hartinger <jharting@redhat.com>
2018-11-29 14:30:45 +0100

    improve diagnosis test case

--- src/diagnosis/diagnosis.controller.js
+++ src/diagnosis/diagnosis.controller.js
@@ -7,25 +7,31 @@ const errors = require('../errors');
  */
 exports.getDiagnosis = errors.async(async function (req, res) {
     const systemId = req.swagger.params.system.value;
+    const remediationId = req.swagger.params.remediation.value;
 
     // TODO:
     // 1) obtain system data from inventory
     // 2) verify the principal is allowed to read the current system
     // 3) get report details from advisor, vulnerabilities
 
-    res.json({
+    const data = {
         diagnosis: {
             'CVE_2018_1111_dhcp|ERROR_CVE_2018_1111_DHCP_2': {
                 dhcp_devs: [
                     'enp0s3'
                 ],
                 system: systemId
-            },
-            'CVE_2017_5753_4_cpu_kernel|KERNEL_CVE_2017_5753_4_CPU_ERROR_3': {
-                debugfs_available: true,
-                dmesg_available: true,
-                dmesg_wrapped: false
             }
         }
-    });
+    };
+
+    if (!remediationId) {
+        data.diagnosis['CVE_2017_5753_4_cpu_kernel|KERNEL_CVE_2017_5753_4_CPU_ERROR_3'] = {
+            debugfs_available: true,
+            dmesg_available: true,
+            dmesg_wrapped: false
+        };
+    }
+
+    res.json(data);
 });
