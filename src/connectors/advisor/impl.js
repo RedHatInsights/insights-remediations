33e7a27312dbb467c80a4ad6365b2d196215f195
Jozef Hartinger <jharting@redhat.com>
2019-01-02 17:49:21 +0100

    forward identity header to advisor

--- src/connectors/advisor/impl.js
+++ src/connectors/advisor/impl.js
@@ -3,22 +3,28 @@
 const request = require('../http');
 const URI = require('urijs');
 const {host, insecure} = require('../../config').advisor;
+const { IDENTITY_HEADER } = require('../../middleware/identity/utils');
+const cls = require('../../util/cls');
+const assert = require('assert');
 
 exports.getRule = function (id) {
     const uri = new URI(host);
     uri.path('/r/insights/platform/advisor/v1/rule/');
     uri.segment(id);
 
+    const req = cls.getReq();
+    assert(req, 'request not available in CLS');
+    const identity = req.headers[IDENTITY_HEADER];
+    assert(req, 'identity header not available for outbound advisor request');
+
     return request({
         uri: uri.toString(),
         method: 'GET',
         json: true,
-        rejectUnauthorized: !insecure
-        /*
+        rejectUnauthorized: !insecure,
         headers: {
-            Authorization: auth
+            [IDENTITY_HEADER]: identity
         }
-        */
     }, true);
 };
 
