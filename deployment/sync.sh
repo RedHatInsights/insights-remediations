199efbb4369bfe7b4827132373296eb3515e4363
Jozef Hartinger <jharting@redhat.com>
2019-01-13 12:08:00 +0100

    migrate deployment configuration to helm

--- deployment/sync.sh
+++ deployment/sync.sh
@@ -4,17 +4,11 @@ set -e
 
 NAMESPACE=$(oc project -q)
 
-if [[ $NAMESPACE != remediations* || $NAMESPACE == "remediations-pr" ]]; then
-    echo "unexpected project: $(oc project -q)"
+if [[ $NAMESPACE != remediations* && $NAMESPACE != "buildfactory" ]]; then
+    echo "unexpected project: $NAMESPACE"
     exit 1;
 fi
 
-HOSTNAME="${NAMESPACE}.5a9f.insights-dev.openshiftapps.com"
-if [[ $NAMESPACE == "remediations-prod" ]]; then
-    HOSTNAME="remediations.1b13.insights.openshiftapps.com"
-fi
+echo "Using values-${NAMESPACE}.yaml"
 
-for i in configuration/*.yaml configuration/cac/*.yaml; do
-    echo "Updating $i"
-    oc process -f $i --local --ignore-unknown-parameters NAMESPACE="$(oc project --short)" HOSTNAME="$HOSTNAME" | oc apply -f -
-done;
+helm template --values="values-${NAMESPACE}.yaml" . | oc apply -f - | grep --color=auto -E 'configured|created|$'
