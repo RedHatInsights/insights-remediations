e22647913838f1df564bab0b615677a3ecf85c32
Jozef Hartinger <jharting@redhat.com>
2019-01-17 11:25:53 +0100

    import certs for backoffice proxy

--- src/connectors/users/impl.js
+++ src/connectors/users/impl.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const request = require('../http');
+const fs = require('fs');
+const path = require('path');
 const URI = require('urijs');
 const _ = require('lodash');
-const {host, insecure, auth, env} = require('../../config').users;
 const assert = require('assert');
+
+const request = require('../http');
+const {host, insecure, auth, env} = require('../../config').users;
+
+const cert = fs.readFileSync(path.resolve(__dirname, '../../../certs/backoffice-proxy.crt'));
+const ca = fs.readFileSync(path.resolve(__dirname, '../../../certs/backoffice-proxy.ca.crt'));
 
 exports.getUsers = async function (ids = []) {
     assert(Array.isArray(ids));
@@ -20,6 +26,8 @@ exports.getUsers = async function (ids = []) {
         uri: uri.toString(),
         method: 'POST',
         json: true,
+        ca,
+        cert,
         rejectUnauthorized: !insecure,
         headers: {
             'x-rh-apitoken': auth,
