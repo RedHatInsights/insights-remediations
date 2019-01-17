17e832a5f5d0feefdc04bb160d970ba3144fdd85
Jozef Hartinger <jharting@redhat.com>
2019-01-17 10:17:33 +0100

    sync up image sync to prod

--- deployment/sync-images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DEV_TOKEN=$1;
-PROD_TOKEN=$2;
-
-skopeo copy --src-creds=jharting:$DEV_TOKEN --dest-creds=jharting:$PROD_TOKEN docker://registry.insights-dev.openshift.com/remediations-ci/remediations:latest docker://registry.insights.openshift.com/remediations-prod/remediations:latest
-skopeo copy --src-creds=jharting:$DEV_TOKEN --dest-creds=jharting:$PROD_TOKEN docker://registry.insights-dev.openshift.com/remediations-ci/playbooks-cac:latest docker://registry.insights.openshift.com/remediations-prod/playbooks-cac:latest
