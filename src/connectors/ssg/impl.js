e0cd29419c6c21e92020959bcb207ed082c42341
Jozef Hartinger <jharting@redhat.com>
2019-02-05 12:41:31 +0100

    revisit ssg connector

--- src/connectors/ssg/impl.js
+++ src/connectors/ssg/impl.js
@@ -2,12 +2,15 @@
 
 const URI = require('urijs');
 const Connector = require('../Connector');
+const metrics = require('../metrics');
+const assert = require('assert');
 
 const {host} = require('../../config').ssg;
 
 module.exports = new class extends Connector {
     constructor () {
         super(module);
+        this.metrics = metrics.createConnectorMetric(this.getName());
     }
 
     getTemplate (id) {
@@ -15,10 +18,11 @@ module.exports = new class extends Connector {
         uri.segment('/playbooks');
         uri.segment(`${id}.yml`);
 
-        return this.doHttp({ uri: uri.toString() }, true);
+        return this.doHttp({ uri: uri.toString() }, false, this.metrics);
     }
 
-    ping () {
-        return this.getTemplate('sshd_disable_root_login');
+    async ping () {
+        const result = await this.getTemplate('sshd_disable_root_login');
+        assert(result !== null);
     }
 }();
