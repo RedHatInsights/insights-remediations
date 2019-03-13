c4a204b6bbd458629face07dee10045e80a65c14
Jozef Hartinger <jharting@redhat.com>
2019-03-13 16:11:33 +0100

    deal with prometheus' existential issues
    https://www.robustperception.io/existential-issues-with-metrics

--- src/metrics/index.js
+++ src/metrics/index.js
@@ -1,8 +1,18 @@
 'use strict';
 
+const _ = require('lodash');
 const promBundle = require('express-prom-bundle');
 
 const { enabled, prefix } = require('../config').metrics;
+
+// https://www.robustperception.io/existential-issues-with-metrics
+function initializeLabels (middleware) {
+    const metric = middleware.metrics.remediations_http_request_duration_seconds;
+    metric.labels(500).observe(0);
+    const label = metric.hashMap['status_code:500'];
+    label.count = 0;
+    label.bucketValues = _.mapValues(label.bucketValues, () => 0);
+}
 
 exports.start = function (app) {
     if (!enabled) {
@@ -23,5 +33,8 @@ exports.start = function (app) {
             }
         }
     });
+
+    initializeLabels(metricsMiddleware);
+
     app.use(metricsMiddleware);
 };
