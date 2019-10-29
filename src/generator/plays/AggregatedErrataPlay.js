4d2c0a0e77e9a7e37083e1a995df2d532bc26d93
Tomas Kasparek <tkasparek@redhat.com>
2019-10-29 14:15:16 +0100

    fix remediation of multiple CVEs on RHEL 8 systems

--- src/generator/plays/AggregatedErrataPlay.js
+++ src/generator/plays/AggregatedErrataPlay.js
@@ -15,6 +15,10 @@ module.exports = class MergedPlay extends Play {
         super(plays[0].id, plays[0].hosts);
         this.plays = _.sortBy(plays, 'erratum');
         this.issues = _.map(this.plays, 'erratum');
+        if (!plays[0].isAdvisory) {
+            this.issues = _.join(this.issues, ' --cve ');
+        }
+
         this.template = plays[0].isAdvisory ? ERRATA_TEMPLATE : CVES_TEMPLATE;
     }
 
