f129795d230759039bf43d8691b82bce891c8535
Alec Cohan <alcohan@redhat.com>
2020-02-05 11:00:21 -0500

    RHCLOUD-4397: Added RBAC permissions to descriptions of each operation

--- src/generator/routes.js
+++ src/generator/routes.js
@@ -4,6 +4,8 @@ const controller = require('./generator.controller');
 const openapi = require('../middleware/openapi');
 const rbac = require('../middleware/rbac');
 
+const rbacRead = rbac('remediations:resolution:read');
+
 module.exports = function (router) {
-    router.post('/playbook', openapi('generate'), rbac('remediations:resolution:read'), controller.generate);
+    router.post('/playbook', openapi('generate'), rbacRead, controller.generate);
 };
