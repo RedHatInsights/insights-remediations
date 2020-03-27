f0bcd0caaccb2683f998356db96c16e0df1c172a
Jozef Hartinger <jharting@redhat.com>
2020-03-27 17:26:36 +0100

    fix the down function of 20200312105619-fifi-tweaks migration to actually deal with null values

--- db/migrations/20200312105619-fifi-tweaks.js
+++ db/migrations/20200312105619-fifi-tweaks.js
@@ -18,6 +18,12 @@ module.exports = {
     },
 
     async down (q, {UUID, STRING}) {
+        q.sequelize.query(
+            // just setting it to an arbitrary id so that it passes the NOT NULL constraint
+            // eslint-disable-next-line max-len
+            `UPDATE "playbook_run_executors" SET "receptor_job_id" = '7cfcf78f-6c17-4ceb-b839-022f8a47b7b1' WHERE "receptor_job_id" IS NULL`
+        );
+
         await q.changeColumn('playbook_run_executors',
             'receptor_job_id', {
                 type: UUID,
