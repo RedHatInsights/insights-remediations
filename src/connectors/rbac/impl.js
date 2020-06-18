84879ed92e4313d19256f45aa41f0f0a4fb64253
Alec Cohan <44471274+aleccohan@users.noreply.github.com>
2020-06-18 12:27:40 -0400

    add retry logic to rbac connector (#377)

--- src/connectors/rbac/impl.js
+++ src/connectors/rbac/impl.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const assert = require('assert');
 const URI = require('urijs');
 const Connector = require('../Connector');
+const log = require('../../util/log');
 
 const { host, insecure } = require('../../config').rbac;
 const metrics = require('../metrics');
@@ -14,24 +15,33 @@ module.exports = new class extends Connector {
         this.accessMetrics = metrics.createConnectorMetric(this.getName(), 'getRemediationsAccess');
     }
 
-    async getRemediationsAccess () {
+    async getRemediationsAccess (retries = 2) {
         const uri = new URI(host);
         uri.path('/api/rbac/v1/access/');
         uri.query({application: 'remediations'});
 
-        const result = await this.doHttp ({
-            uri: uri.toString(),
-            method: 'GET',
-            json: true,
-            rejectUnauthorized: !insecure,
-            headers: this.getForwardedHeaders()
-        }, false, this.accessMetrics);
+        try {
+            const result = await this.doHttp ({
+                uri: uri.toString(),
+                method: 'GET',
+                json: true,
+                rejectUnauthorized: !insecure,
+                headers: this.getForwardedHeaders()
+            }, false, this.accessMetrics);
 
-        if (_.isEmpty(result)) {
-            return null;
+            if (_.isEmpty(result)) {
+                return null;
+            }
+
+            return result;
+        } catch (e) {
+            if (retries > 0) {
+                log.warn({ error: e, retries }, 'RBAC access fetch failed. Retrying');
+                return this.getRemediationsAccess(retries - 1);
+            }
+
+            throw e;
         }
-
-        return result;
     }
 
     async ping () {
--- src/connectors/rbac/impl.unit.js
+++ src/connectors/rbac/impl.unit.js
@@ -6,6 +6,7 @@ const Connector = require('../Connector');
 const { mockRequest, mockCache } = require('../testUtils');
 const request = require('../../util/request');
 const errors = require('../../errors');
+const RequestError = require('request-promise-core/errors').RequestError;
 
 describe('rbac impl', function () {
     beforeEach(mockRequest);
@@ -53,6 +54,64 @@ describe('rbac impl', function () {
         result.data[0].should.have.property('permission', 'remediations:*:*');
         result.data[0].should.have.property('resourceDefinitions', []);
 
+        const options = http.args[0][0];
+        options.uri.should.equal('http://localhost:8080/api/rbac/v1/access/?application=remediations');
+        options.headers.should.have.size(2);
+        options.headers.should.have.property('x-rh-insights-request-id', 'request-id');
+        options.headers.should.have.property('x-rh-identity', 'identity');
+
+        cache.get.callCount.should.equal(0);
+        cache.setex.callCount.should.equal(0);
+    });
+
+    test('retries when operation errors out', async function () {
+        const cache = mockCache();
+        const http = base.getSandbox().stub(request, 'run');
+
+        http.onFirstCall().rejects(new RequestError('Error: socket hang up'));
+        http.onSecondCall().rejects(new RequestError('Error: socket hang up'));
+        http.resolves({
+            statusCode: 200,
+            body: {
+                meta: {
+                    count: 1,
+                    limit: 10,
+                    offset: 0
+                },
+                links: {
+                    first: '/api/rbac/v1/access/?application=remediations&limit=10&offset=0',
+                    next: null,
+                    previous: null,
+                    last: '/api/rbac/v1/access/?application=remediations&limit=10&offset=0'
+                },
+                data: [
+                    {
+                        permission: 'remediations:*:*',
+                        resourceDefinitions: []
+                    }
+                ]
+            },
+            headers: {}
+        });
+
+        const result = await impl.getRemediationsAccess();
+        result.meta.should.have.property('count', 1);
+        result.meta.should.have.property('limit', 10);
+        result.meta.should.have.property('offset', 0);
+
+        result.links.should.have.property(
+            'first', '/api/rbac/v1/access/?application=remediations&limit=10&offset=0'
+        );
+        result.links.should.have.property('next', null);
+        result.links.should.have.property('previous', null);
+        result.links.should.have.property(
+            'last', '/api/rbac/v1/access/?application=remediations&limit=10&offset=0'
+        );
+
+        result.data[0].should.have.property('permission', 'remediations:*:*');
+        result.data[0].should.have.property('resourceDefinitions', []);
+
+        http.callCount.should.equal(3);
         const options = http.args[0][0];
         options.uri.should.equal('http://localhost:8080/api/rbac/v1/access/?application=remediations');
         options.headers.should.have.size(2);
