7010aaea9751f9815ec364f12895180810ba8657
Alec Cohan <44471274+aleccohan@users.noreply.github.com>
2021-04-08 09:38:09 -0400

    RHCLOUD-13324: Update dispatcher-connector with new API endpoints (#25)

--- src/connectors/dispatcher/impl.unit.js
+++ src/connectors/dispatcher/impl.unit.js
@@ -79,4 +79,179 @@ describe('dispatcher impl', function () {
             await expect(impl.postPlaybookRunRequests(DISPATCHERWORKREQUEST)).rejects.toThrow(errors.DependencyError);
         });
     });
+
+    describe('getPlaybookRuns', function () {
+        test('get list of runs', async function () {
+            const http = base.getSandbox().stub(request, 'run').resolves({
+                statusCode: 200,
+                body: {
+                    meta: {
+                        count: 2
+                    },
+                    data: [
+                        {
+                            id: '8e015e92-02bd-4df1-80c5-3a00b93c4a4a',
+                            account: 654321,
+                            recipient: '9574cba7-b9ce-4725-b392-e959afd3e69a',
+                            correlation_id: '5c9ae28b-1728-4067-b1f3-f4ad992a8296',
+                            url: 'https://cloud.redhat.com/api/remediations/v1/remediations/f376d664-5725-498d-8cf9-bbfaa51b80ca/playbook?hosts=9574cba7-b9ce-4725-b392-e959afd3e69a&localhost',
+                            labels: {
+                                'playbook-run': 'ef7a1724-6adc-4370-b88c-bed7cb2d3fd2'
+                            },
+                            status: 'running',
+                            service: 'remediations',
+                            created_at: 'sometime',
+                            updated_at: 'sometime'
+                        },
+                        {
+                            id: '9ce94170-34a0-4aa6-976a-9728aa4da7a4',
+                            account: 654321,
+                            recipient: '750c60ee-b67e-4ccd-8d7f-cb8aed2bdbf4',
+                            correlation_id: '1b4244aa-2572-4067-bf44-ad4e5bfaafc4',
+                            url: 'https://cloud.redhat.com/api/remediations/v1/remediations/f376d664-5725-498d-8cf9-bbfaa51b80ca/playbook?hosts=750c60ee-b67e-4ccd-8d7f-cb8aed2bdbf4&localhost',
+                            labels: {
+                                'playbook-run': 'fe7a1724-6adc-4370-b88c-bed7cb2d3fd4'
+                            },
+                            status: 'running',
+                            service: 'remediations',
+                            created_at: 'sometime',
+                            updated_at: 'sometime'
+                        }
+                    ]
+                },
+                headers: {}
+            });
+
+            const results = await impl.fetchPlaybookRuns();
+            results.data.should.have.size(2);
+
+            const result1 = results.data[0];
+            result1.should.have.property('id', '8e015e92-02bd-4df1-80c5-3a00b93c4a4a');
+            result1.should.have.property('account', 654321);
+            result1.should.have.property('recipient', '9574cba7-b9ce-4725-b392-e959afd3e69a');
+            result1.should.have.property('correlation_id', '5c9ae28b-1728-4067-b1f3-f4ad992a8296');
+            result1.should.have.property('url', 'https://cloud.redhat.com/api/remediations/v1/remediations/f376d664-5725-498d-8cf9-bbfaa51b80ca/playbook?hosts=9574cba7-b9ce-4725-b392-e959afd3e69a&localhost');
+            result1.should.have.property('labels', {'playbook-run': 'ef7a1724-6adc-4370-b88c-bed7cb2d3fd2'});
+            result1.should.have.property('status', 'running');
+            result1.should.have.property('service', 'remediations');
+            result1.should.have.property('created_at', 'sometime');
+            result1.should.have.property('updated_at', 'sometime');
+
+            const result2 = results.data[1];
+            result2.should.have.property('id', '9ce94170-34a0-4aa6-976a-9728aa4da7a4');
+            result2.should.have.property('account', 654321);
+            result2.should.have.property('recipient', '750c60ee-b67e-4ccd-8d7f-cb8aed2bdbf4');
+            result2.should.have.property('correlation_id', '1b4244aa-2572-4067-bf44-ad4e5bfaafc4');
+            result2.should.have.property('url', 'https://cloud.redhat.com/api/remediations/v1/remediations/f376d664-5725-498d-8cf9-bbfaa51b80ca/playbook?hosts=750c60ee-b67e-4ccd-8d7f-cb8aed2bdbf4&localhost');
+            result2.should.have.property('labels', {'playbook-run': 'fe7a1724-6adc-4370-b88c-bed7cb2d3fd4'});
+            result2.should.have.property('status', 'running');
+            result2.should.have.property('service', 'remediations');
+            result2.should.have.property('created_at', 'sometime');
+            result2.should.have.property('updated_at', 'sometime');
+
+            http.callCount.should.equal(1);
+            const options = http.args[0][0];
+            options.headers.should.have.size(2);
+            options.headers.should.have.property('x-rh-insights-request-id', 'request-id');
+            options.headers.should.have.property('x-rh-identity', 'identity');
+        });
+
+        test('returns null dispatcherWorkRequest is incorrect', async function () {
+            base.getSandbox().stub(Connector.prototype, 'doHttp').resolves([]);
+            await expect(impl.fetchPlaybookRuns()).resolves.toBeNull();
+        });
+
+        test('connection error handling dispatcherWorkRequest', async function () {
+            base.mockRequestError();
+            await expect(impl.fetchPlaybookRuns()).rejects.toThrow(errors.DependencyError);
+        });
+
+        test('status code handling dispatcherWorkRequest', async function () {
+            base.mockRequestStatusCode();
+            await expect(impl.fetchPlaybookRuns()).rejects.toThrow(errors.DependencyError);
+        });
+    });
+
+    describe('getPlaybookRunHosts', function () {
+        test('get list of run hosts', async function () {
+            const http = base.getSandbox().stub(request, 'run').resolves({
+                statusCode: 200,
+                body: {
+                    meta: {
+                        count: 2
+                    },
+                    data: [
+                        {
+                            host: '9574cba7-b9ce-4725-b392-e959afd3e69a',
+                            run: {
+                                id: '8e015e92-02bd-4df1-80c5-3a00b93c4a4a',
+                                account: 654321,
+                                recipient: '9574cba7-b9ce-4725-b392-e959afd3e69a',
+                                correlation_id: '5c9ae28b-1728-4067-b1f3-f4ad992a8296',
+                                url: 'https://cloud.redhat.com/api/remediations/v1/remediations/f376d664-5725-498d-8cf9-bbfaa51b80ca/playbook?hosts=9574cba7-b9ce-4725-b392-e959afd3e69a&localhost',
+                                labels: {
+                                    'playbook-run': 'ef7a1724-6adc-4370-b88c-bed7cb2d3fd2'
+                                },
+                                timeout: '2000',
+                                status: 'running'
+                            },
+                            status: 'running',
+                            stdout: 'console log goes here'
+                        },
+                        {
+                            host: '750c60ee-b67e-4ccd-8d7f-cb8aed2bdbf4',
+                            run: {
+                                id: '9ce94170-34a0-4aa6-976a-9728aa4da7a4',
+                                account: 654321,
+                                recipient: '750c60ee-b67e-4ccd-8d7f-cb8aed2bdbf4',
+                                url: 'https://cloud.redhat.com/api/remediations/v1/remediations/f376d664-5725-498d-8cf9-bbfaa51b80ca/playbook?hosts=750c60ee-b67e-4ccd-8d7f-cb8aed2bdbf4&localhost',
+                                labels: {
+                                    'playbook-run': 'fe7a1724-6adc-4370-b88c-bed7cb2d3fd4'
+                                },
+                                timeout: '2000',
+                                status: 'running'
+                            },
+                            status: 'running',
+                            stdout: 'console log goes here'
+                        }
+                    ]
+                },
+                headers: {}
+            });
+
+            const results = await impl.fetchPlaybookRunHosts();
+            results.data.should.have.size(2);
+
+            const result1 = results.data[0];
+            result1.should.have.property('host', '9574cba7-b9ce-4725-b392-e959afd3e69a');
+            result1.should.have.property('status', 'running');
+            result1.should.have.property('stdout', 'console log goes here');
+
+            const result2 = results.data[1];
+            result2.should.have.property('host', '750c60ee-b67e-4ccd-8d7f-cb8aed2bdbf4');
+            result2.should.have.property('status', 'running');
+            result2.should.have.property('stdout', 'console log goes here');
+
+            http.callCount.should.equal(1);
+            const options = http.args[0][0];
+            options.headers.should.have.size(2);
+            options.headers.should.have.property('x-rh-insights-request-id', 'request-id');
+            options.headers.should.have.property('x-rh-identity', 'identity');
+        });
+
+        test('returns null dispatcherWorkRequest is incorrect', async function () {
+            base.getSandbox().stub(Connector.prototype, 'doHttp').resolves([]);
+            await expect(impl.fetchPlaybookRunHosts()).resolves.toBeNull();
+        });
+
+        test('connection error handling dispatcherWorkRequest', async function () {
+            base.mockRequestError();
+            await expect(impl.fetchPlaybookRunHosts()).rejects.toThrow(errors.DependencyError);
+        });
+
+        test('status code handling dispatcherWorkRequest', async function () {
+            base.mockRequestStatusCode();
+            await expect(impl.fetchPlaybookRunHosts()).rejects.toThrow(errors.DependencyError);
+        });
+    });
 });
