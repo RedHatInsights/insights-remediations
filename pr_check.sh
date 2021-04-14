172414c994666bdbae8e5bc6c1c832b5ce002440
Stephen Adams <tsadams@gmail.com>
2021-04-14 16:22:22 -0400

    [fix] export IMAGE in the pr_check script
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- pr_check.sh
+++ pr_check.sh
@@ -2,6 +2,8 @@
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source ./.cicd_bootstrap.sh
 
+export IMAGE="quay.io/cloudservices/insights-remediations"
+
 # build the PR commit change
 source $CICD_ROOT/build.sh
 
