e0b91e3782f38126e9df5e701582c7eea286cd86
Stephen Adams <tsadams@gmail.com>
2021-04-14 15:58:26 -0400

    [fix] use absolute path of bootstrap file
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- pr_check.sh
+++ pr_check.sh
@@ -1,6 +1,6 @@
 # Get bonfire helper scripts
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source ./.cicd_bootstrap.sh
 
 # build the PR commit change
 source $CICD_ROOT/build.sh
