a1b713087c02bd5be860d824c6f3b0f28f3e1742
Stephen Adams <tsadams@gmail.com>
2021-04-14 16:33:20 -0400

    [fix] add required vars
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- pr_check.sh
+++ pr_check.sh
@@ -3,6 +3,8 @@ CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source ./.cicd_bootstrap.sh
 
 export IMAGE="quay.io/cloudservices/remediations"
+export COMPONENT_NAME="remediatons"
+export APP_NAME="remediations"
 
 # build the PR commit change
 source $CICD_ROOT/build.sh
