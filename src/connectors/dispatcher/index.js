b948c03a6636e79b7745e8f557638a0551a88ef5
Jozef Hartinger <jharting@redhat.com>
2021-05-07 08:55:28 +0200

    add noop playbook-dispatcher connector

--- src/connectors/dispatcher/index.js
+++ src/connectors/dispatcher/index.js
@@ -5,6 +5,8 @@ const config = require('../../config');
 
 if (useMock(config.dispatcher.impl)) {
     module.exports = require('./mock');
+} else if (config.dispatcher.impl === 'noop') {
+    module.exports = require('./noop');
 } else {
     module.exports = require('./impl');
 }
