c17105f200d13f079f5003a88ae60a6333adbe9b
Alec Cohan <alcohan@redhat.com>
2021-06-08 15:57:31 -0400

    Add additional comments to pr_check

--- pr_check.sh
+++ pr_check.sh
@@ -15,6 +15,5 @@ IQE_FILTER_EXPRESSION=""
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
+# Build Remediations image based on the latest commit
 source $CICD_ROOT/build.sh
-
-# TODO: add unit testing here??
