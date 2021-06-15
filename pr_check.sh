fac0e320833b5c480d8d1eac0858f9ceb7c584a7
Alec Cohan <alcohan@redhat.com>
2021-06-15 11:24:37 -0400

    Add unit_test.sh to remediations & Clowder pipeline

--- pr_check.sh
+++ pr_check.sh
@@ -5,11 +5,12 @@
 # --------------------------------------------
 APP_NAME="remediations"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="remediations"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
-IMAGE="quay.io/cloudservices/insights-remediations"  
+IMAGE="quay.io/cloudservices/insights-remediations"
 
-IQE_PLUGINS="remediations"
-IQE_MARKER_EXPRESSION="smoke"
-IQE_FILTER_EXPRESSION=""
+# ADD IQE TESTS IN LATER
+# IQE_PLUGINS="remediations"
+# IQE_MARKER_EXPRESSION="smoke"
+# IQE_FILTER_EXPRESSION=""
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
@@ -17,3 +18,6 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 # Build Remediations image based on the latest commit
 source $CICD_ROOT/build.sh
+source $CICD_ROOT//deploy_ephemeral_env.sh
+
+source $APP_ROOT/unit_test.sh
