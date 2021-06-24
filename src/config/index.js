c6edbcfe52d429f6f4da11ff9785cfc23490a715
Alec Cohan <44471274+aleccohan@users.noreply.github.com>
2021-06-24 09:37:45 -0400

    Add additional clowdapp changes to clowdapp (#83)
    * Add additional clowdapp changes to clowdapp
    
    * additional fixes to index.js

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const _ = require('lodash');
 const env = process.env;
 
-const ClowderEnabled = (process.env.CLOWDER_ENABLED === true) ? true : false;
+const ClowderEnabled = (process.env.CLOWDER_ENABLED === 'true') ? true : false;
 
 /* eslint-disable max-len*/
 /* eslint no-process-env: off */
