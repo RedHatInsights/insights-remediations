ee19a4a6cdf417aada7714819b30d82f9a766710
Alec Cohan <alcohan@redhat.com>
2021-08-26 14:36:39 -0400

    update pr_check for app-interface

--- unit_test.sh
+++ unit_test.sh
@@ -13,7 +13,7 @@ npm run db:ims
 npm run verify
 result=$?
 
-# TODO: add unittest-xml-reporting to rbac so that junit results can be parsed by jenkins
+# TODO: add unittest-xml-reporting to rbac so that junit results can be parsed by jenkins.
 mkdir -p $WORKSPACE/artifacts
 cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
 <testsuite tests="1">
