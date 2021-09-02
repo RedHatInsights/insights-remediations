d9d4b4c5e2e069e6da52ff9789a6b170c32b716c
Alec Cohan <44471274+aleccohan@users.noreply.github.com>
2021-09-02 14:03:40 -0400

    Update app common js lib (#130)
    * Update app common js lib
    
    * Remove console logs from config index
    
    * Update receptor clowder service to point to gateway clowder

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
 const env = process.env;
-const console = require('console');
 
 const acgConfig = env.ACG_CONFIG;
 
@@ -231,11 +230,9 @@ function Config() {
         config.contentServer.host = getHostForApp(dependencyEndpoints, 'advisor', 'service') || env.CONTENT_SERVER_HOST || 'http://insights-advisor-api.advisor-ci.svc.cluster.local:8000';
         config.dispatcher.host = getHostForApp(dependencyEndpoints, 'playbook-dispatcher', 'api') || env.PLAYBOOK_DISPATCHER_HOST || 'http://playbook-dispatcher-api.playbook-dispatcher-ci.svc.cluster.local:8000';
         config.inventory.host = getHostForApp(dependencyEndpoints, 'host-inventory', 'service') || env.INVENTORY_HOST || 'http://insights-inventory.platform-ci.svc.cluster.local:8080';
-        config.patchman.host = getHostForApp(dependencyEndpoints, 'patchman', 'patchman-manager') || env.PATCHMAN_HOST || 'http://localhost:8080';
-        console.log('Patchman object: ', dependencyEndpoints.patchman);
-        console.log('Inventory object: ', dependencyEndpoints['host-inventory']);
+        config.patchman.host = getHostForApp(dependencyEndpoints, 'patchman', 'manager') || env.PATCHMAN_HOST || 'http://localhost:8080';
         config.rbac.host = getHostForApp(dependencyEndpoints, 'rbac', 'service') || env.RBAC_HOST || 'http://localhost:8080';
-        config.receptor.host = getHostForApp(dependencyEndpoints, 'receptor', 'service') || env.RECEPTOR_HOST || 'http://localhost:9090';
+        config.receptor.host = getHostForApp(dependencyEndpoints, 'receptor', 'gateway-clowder') || env.RECEPTOR_HOST || 'http://localhost:9090';
         config.sources.host = getHostForApp(dependencyEndpoints, 'sources-api', 'svc') || env.SOURCES_HOST || 'http://localhost:8080';
         config.ssg.host = getHostForApp(privateDepencencyEndpoints, 'compliance-ssg', 'service') || env.SSG_HOST || 'http://localhost:8090';
         config.vmaas.host = getHostForApp(dependencyEndpoints, 'vmaas', 'webapp') || env.VMAAS_HOST || 'https://webapp-vmaas-prod.apps.crcp01ue1.o9m8.p1.openshiftapps.com';
