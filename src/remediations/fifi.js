c74030f5af80dadab06a9d21c58f0e87de4d1d9b
Alec Cohan <alcohan@redhat.com>
2022-04-07 09:51:47 -0400

    Update preflight with fix for 500 error codes in stage

--- src/remediations/fifi.js
+++ src/remediations/fifi.js
@@ -269,14 +269,18 @@ async function defineDirectConnectedRHCSystems (executor, org_id) {
     const rhcSystems = _.partition(executor.systems, system => !_.isUndefined(system.rhc_client));
     const dispatcherStatusRequest = _.map(rhcSystems[0], system => ({ recipient: system.rhc_client, org_id }));
 
-    log.info('created dispatcher status request: ', dispatcherStatusRequest);
+    log.info(`created dispatcher status request: ${dispatcherStatusRequest}`);
 
     const requestStatuses = await dispatcher.getPlaybookRunRecipientStatus(dispatcherStatusRequest);
 
     // partition systems containing rhc_client_ids by connection status
     _.forEach(rhcSystems[0], system => {
-        if (requestStatuses[system.rhc_client]) {
-            system.rhcStatus = requestStatuses[system.rhc_client].connected;
+        if (!_.isNull(requestStatuses)) {
+            if (requestStatuses[system.rhc_client]) {
+                system.rhcStatus = requestStatuses[system.rhc_client].connected;
+            } else {
+                system.rhcStatus = false;
+            }
         } else {
             system.rhcStatus = false;
         }
