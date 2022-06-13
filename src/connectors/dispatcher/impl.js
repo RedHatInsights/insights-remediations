f2d4a2628d208a8f76fcee6474efce922bde168b
Rex White <rexwhite@redhat.com>
2022-06-13 15:15:11 -0400

    Fixed a few linting issues.

--- src/connectors/dispatcher/impl.js
+++ src/connectors/dispatcher/impl.js
@@ -7,7 +7,7 @@ const {host, insecure, auth} = require('../../config').dispatcher;
 
 const Connector = require('../Connector');
 const metrics = require('../metrics');
-const log = require('../../util/log')
+const log = require('../../util/log');
 
 const QSOPTIONS = { encode: true, indices: false };
 
@@ -81,9 +81,9 @@ module.exports = new class extends Connector {
             };
         }
 
-        log.info({'postDetails': options}, 'V2PlaybookRunRequests POST');
+        log.info({postDetails: options}, 'V2PlaybookRunRequests POST');
         const result = await this.doHttp (options, false, this.postRunRequests);
-        log.info({'result': result}, 'V2PlaybookRunRequests results');
+        log.info({result: result}, 'V2PlaybookRunRequests results');
 
         if (_.isEmpty(result)) {
             return null;
@@ -192,7 +192,7 @@ module.exports = new class extends Connector {
 
         log.info({request: dispatcherStatusRequest}, 'PRE RunRecipientStatus');
         const result = await this.doHttp (options, false, this.getRunRecipientStatus);
-        log.info({result: result}, 'POST RunRecipientStatus')
+        log.info({result: result}, 'POST RunRecipientStatus');
 
         if (_.isNull(result)) {
             return null;
