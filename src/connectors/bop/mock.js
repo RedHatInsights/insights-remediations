16565986dad73fdd3e8099553003776ce066377b
Rex White <rexwhite@redhat.com>
2022-08-25 16:07:49 -0400

    Tenant org_id migration - db migrations (#236)
    * Fixed BOP url
    
    * Added migration to pipulate tenant_org_id columns in database

--- src/connectors/bop/mock.js
+++ src/connectors/bop/mock.js
@@ -8,6 +8,15 @@ const ORG_IDS = {
     "3098430": "38393949"
 };
 
+const ORG_IDS_2 = {
+    "demo": "1",
+    "test": "2",
+    "fifi": "3",
+    "testStatus": "4",
+    "testWrite": "5",
+    "testReadSingle": "6"
+};
+
 const EBS_ACCOUNTS = {
     "29393933": "2828282",
     "1979710": "540155",
@@ -20,7 +29,7 @@ module.exports = new class extends Connector {
     }
 
     async getTenantOrgIds() {
-        return ORG_IDS;
+        return ORG_IDS_2;
     }
 
     async getEBSAccounts() {
