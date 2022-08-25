4500de52e514c751ff31f8d4cdd19b13e73a3580
Rex White <rexwhite@redhat.com>
2022-08-25 16:02:08 -0400

    Added migration to pipulate tenant_org_id columns in database

---
+++ db/migrations/20220825014939-populate-tenant_org_id.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const _ = require('lodash');
+const bop = require('../../src/connectors/bop');
+
+module.exports = {
+  up: async (q, Sequelize) => {
+    //===================================================
+    // populate tenant_org_id for remediations table
+    //===================================================
+    {
+      // get unique account numbers
+      const query = "SELECT DISTINCT account_number FROM remediations WHERE tenant_org_id IS NULL";
+      const [results, metadata] = await q.sequelize.query(query);
+      const EBS_accounts = _(results).map('account_number').value();
+
+      // get tenant_org_id map
+      const org_ids = await bop.getTenantOrgIds(EBS_accounts);
+
+      // populate tenant_org_id
+      for (const account in org_ids) {
+        const query = `UPDATE remediations SET tenant_org_id = '${org_ids[account]}' WHERE account_number = '${account}'`
+        q.sequelize.query(query);
+      }
+    }
+
+    //===================================================
+    // populate tenant_org_id for playbook_archive table
+    //===================================================
+    {
+      // get unique account numbers
+      const query = "SELECT DISTINCT account_number FROM playbook_archive WHERE tenant_org_id IS NULL";
+      const [results, metadata] = await q.sequelize.query(query);
+      const EBS_accounts = _(results).map('account_number').value();
+
+      // get tenant_org_id map
+      const org_ids = await bop.getTenantOrgIds(EBS_accounts);
+
+      // populate tenant_org_id
+      for (const account in org_ids) {
+        const query = `UPDATE playbook_archive SET tenant_org_id = '${org_ids[account]}' WHERE account_number = '${account}'`
+        q.sequelize.query(query);
+      }
+    }
+  },
+
+  down: async (q, Sequelize) => {
+    //===================================================
+    // zero out tenant_org_id for remediations table
+    //===================================================
+    q.sequelize.query("UPDATE remediations SET tenant_org_id = NULL")
+
+    //===================================================
+    // zero out tenant_org_id for playbook_archive table
+    //===================================================
+    q.sequelize.query("UPDATE playbook_archive SET tenant_org_id = NULL")
+  }
+};
