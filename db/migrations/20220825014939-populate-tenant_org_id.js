fffed6be3e19f2c4450a2e7d4cbf69d72f21ac5a
Rex White <rexwhite@redhat.com>
2022-08-31 14:15:31 -0400

    Changed migration to support chunking

--- db/migrations/20220825014939-populate-tenant_org_id.js
+++ db/migrations/20220825014939-populate-tenant_org_id.js
@@ -3,6 +3,8 @@
 const _ = require('lodash');
 const bop = require('../../src/connectors/bop');
 
+const BATCH_SIZE = 50;
+
 module.exports = {
   up: async (q, Sequelize) => {
     //===================================================
@@ -12,15 +14,19 @@ module.exports = {
       // get unique account numbers
       const query = "SELECT DISTINCT account_number FROM remediations WHERE tenant_org_id IS NULL";
       const [results, metadata] = await q.sequelize.query(query);
-      const EBS_accounts = _(results).map('account_number').value();
+      const EBS_batch_accounts = _(results).map('account_number').chunk(BATCH_SIZE).value();
 
-      // get tenant_org_id map
-      const org_ids = await bop.getTenantOrgIds(EBS_accounts);
 
-      // populate tenant_org_id
-      for (const account in org_ids) {
-        const query = `UPDATE remediations SET tenant_org_id = '${org_ids[account]}' WHERE account_number = '${account}'`
-        q.sequelize.query(query);
+      // batch org_id lookups....
+      for (let EBS_accounts of EBS_batch_accounts) {
+        // get tenant_org_id map
+        const org_ids = await bop.getTenantOrgIds(EBS_accounts);
+
+        // populate tenant_org_id
+        for (const account in org_ids) {
+          const query = `UPDATE remediations SET tenant_org_id = '${org_ids[account]}' WHERE account_number = '${account}'`
+          q.sequelize.query(query);
+        }
       }
     }
 
@@ -31,15 +37,18 @@ module.exports = {
       // get unique account numbers
       const query = "SELECT DISTINCT account_number FROM playbook_archive WHERE tenant_org_id IS NULL";
       const [results, metadata] = await q.sequelize.query(query);
-      const EBS_accounts = _(results).map('account_number').value();
+      const EBS_batch_accounts = _(results).map('account_number').chunk(BATCH_SIZE).value();
 
-      // get tenant_org_id map
-      const org_ids = await bop.getTenantOrgIds(EBS_accounts);
+      // batch org_id lookuos...
+      for (let EBS_accounts of EBS_batch_accounts) {
+        // get tenant_org_id map
+        const org_ids = await bop.getTenantOrgIds(EBS_accounts);
 
-      // populate tenant_org_id
-      for (const account in org_ids) {
-        const query = `UPDATE playbook_archive SET tenant_org_id = '${org_ids[account]}' WHERE account_number = '${account}'`
-        q.sequelize.query(query);
+        // populate tenant_org_id
+        for (const account in org_ids) {
+          const query = `UPDATE playbook_archive SET tenant_org_id = '${org_ids[account]}' WHERE account_number = '${account}'`
+          q.sequelize.query(query);
+        }
       }
     }
   },
