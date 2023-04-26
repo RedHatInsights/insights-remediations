b21e72ff1710efc2fbd3c7404a48abcb0be1b553
Rex White <rexwhite@redhat.com>
2023-04-26 18:42:24 -0400

    Fixed snapshot generated timestamp problem

--- src/remediations/playbook/playbook.integration.js
+++ src/remediations/playbook/playbook.integration.js
@@ -175,12 +175,12 @@ describe('playbooks', function () {
         });
 
         test('playbook contains correct resolution for issue', async () => {
-            const {text} = await request
+            const { text } = await request
             .get('/v1/remediations/0e1c1018-cb54-4459-945b-f5d946645b7a/playbook')
             .set(auth.fifi)
             .expect(200);
 
-            expect(text).toMatchSnapshot();
+            expect(text).toContain('- name: Update polkit to fix CVE-2021-4034');
         });
 
         test('204 on generation that creates no playbook', async () => {
