babdda49de4a59ea55c9fae07eeb06586e7b9c6f
Rex White <rexwhite@redhat.com>
2023-07-24 16:51:05 -0400

    REMEDY-389: Still trying to figure this out! (#376)

--- src/util/log.js
+++ src/util/log.js
@@ -88,7 +88,7 @@ const serializers = {
                 req.trace.force ||                         // forced tracing
                 elapsed > req.trace.threshold_ms ||  // time limit exceeded
                 value.statusCode >= 400                    // an error condition
-            ) {
+            ){
                 // limit trace message size!  Since the error is most likely to
                 // be at the end, drop characters from the front...
                 const trace_message = req.trace.toString();
@@ -99,6 +99,8 @@ const serializers = {
                     console.log(`Trace data truncated for request: ${req.id}`);
                     console.log(`(trace message length: ${trace_message.length})`);
                 }
+
+                value.trace = '(trace data goes here!)';
             }
         }
 
