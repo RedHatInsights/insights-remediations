05cdc0b71e639b9f906b6674159b4ff3e25f42d6
Rex White <rexwhite@redhat.com>
2023-11-20 16:34:00 -0500

    Added getSystemInfoBatch method to inventory connector

--- src/connectors/inventory/impl.js
+++ src/connectors/inventory/impl.js
@@ -10,6 +10,8 @@ const Connector = require('../Connector');
 const metrics = require('../metrics');
 const log = require('../../util/log');
 
+const SATELLITE_NAMESPACE = 'satellite';
+
 function validateHost (host) {
     assert(_.has(host, 'id'), 'id missing for host');
     assert(_.has(host, 'display_name'), 'display_name missing for host');
@@ -38,6 +40,77 @@ module.exports = new class extends Connector {
 
         // TODO: remove once everything is on cloud.redhat.com
         return this.buildUri(host, 'inventory', 'api', 'v1', 'hosts');
+    }
+
+    async getSystemInfoBatch (ids = [], refresh = false, retries = 2) {
+        if (ids.length === 0) {
+            return {};
+        }
+
+        if (ids.length > pageSize) {
+            const chunks = _.chunk(ids, pageSize);
+            const results = await P.map(chunks, chunk => this.getSystemInfoBatch(chunk, refresh));
+            return _.assign({}, ...results);
+        }
+
+        const uri = this.buildHostsUri();
+        uri.segment(ids.join());
+        uri.addQuery('fields[system_profile]', ['is_marketplace', 'satellite_managed', 'rhc_client_id']);
+        uri.addQuery('per_page', String(pageSize));
+
+        let response = null;
+
+        try {
+            response = await this.doHttp({
+                    uri: uri.toString(),
+                    method: 'GET',
+                    json: true,
+                    rejectUnauthorized: !insecure,
+                    headers: this.getForwardedHeaders()
+                },
+                {
+                    key: `remediations|http-cache|inventory|${ids.join()}`,
+                    refresh,
+                    revalidationInterval,
+                    cacheable: body => body.count > 0 // only cache responses with at least 1 record
+                },
+                this.detailsMetrics);
+        } catch (e) {
+            if (retries > 0) {
+                log.warn({ error: e, ids, retries }, 'Inventory fetch failed. Retrying');
+                return this.getSystemInfoBatch(ids, true, retries - 1);
+            }
+
+            throw e;
+        }
+
+        const transformed = _(response.results)
+            .keyBy('id')
+            .mapValues(system => {
+                const satelliteFacts = _(system.facts).find(
+                    fact => ((fact?.namespace === SATELLITE_NAMESPACE) ? fact.facts : false)
+                )?.facts;
+
+                return {
+                    id: system.id,
+                    display_name: system.display_name,
+                    hostname: system.fqdn,
+                    ansible_host: system.ansible_host,
+                    facts: system.facts,                                         // TODO: do we need this?
+                    satelliteId: satelliteFacts?.satellite_instance_id || null,
+                    satelliteOrgId: satelliteFacts?.organization_id || null,
+                    satelliteVersion: satelliteFacts?.satellite_version || null, // TODO: we don't need this
+                    rhc_client:  system.system_profile?.rhc_client_id || null,
+                    marketplace: system.system_profile?.is_marketplace || false,
+                    satelliteManaged: system.system_profile?.satellite_managed || false
+                };
+            })
+            .value();
+
+        // ensure id, display_name and hostname are present
+        const result = validate(transformed);
+
+        return result;
     }
 
     async getSystemDetailsBatch (ids = [], refresh = false, retries = 2) {
--- src/connectors/inventory/impl.unit.js
+++ src/connectors/inventory/impl.unit.js
@@ -9,6 +9,7 @@ const Connector = require('../Connector');
 const { mockRequest, mockCache } = require('../testUtils');
 const request = require('../../util/request');
 const RequestError = require('request-promise-core/errors').RequestError;
+const inventory_GET = require('./inventory_GET.json');
 
 function inventoryResponse (results, total = results.length) {
     return {
@@ -20,208 +21,229 @@ function inventoryResponse (results, total = results.length) {
 describe('inventory impl', function () {
     beforeEach(mockRequest);
 
-    test('does not make a call for empty list', async function () {
-        const spy = base.getSandbox().spy(http, 'request');
-        const result = await impl.getSystemDetailsBatch([]);
+    describe('getSystemInfoBatch', function () {
+        test('returns expected data', async function () {
+            const EXPECTED_PROPS = ['id', 'display_name', 'hostname', 'ansible_host', 'facts',
+                'satelliteId', 'satelliteOrgId', 'rhc_client', 'marketplace', 'satelliteManaged']; // TODO: remove facts, satelliteVersion?
 
-        result.should.be.empty();
-        spy.called.should.be.false();
+            const spy = base.getSandbox().stub(Connector.prototype, 'doHttp').resolves(inventory_GET);
+
+            const result = await impl.getSystemInfoBatch([
+                "9cc31c02-96f9-4d33-8d14-ea8467393b51",
+                "2c9284a9-a489-43d4-a7bb-1ef43cf890b8"
+            ]);
+
+            result.should.have.size(2);
+            for (const item of Object.values(result)) {
+                item.should.have.properties(EXPECTED_PROPS);
+            }
+        });
     });
 
-    test('forwards request headers', async function () {
-        const spy = base.getSandbox().stub(Connector.prototype, 'doHttp').resolves(inventoryResponse([]));
+    describe('getSystemDetailsBatch', function () {
+        test('does not make a call for empty list', async function () {
+            const spy = base.getSandbox().spy(http, 'request');
+            const result = await impl.getSystemDetailsBatch([]);
 
-        await impl.getSystemDetailsBatch(['id']);
-        const headers = spy.args[0][0].headers;
-        headers.should.have.size(2);
-        headers.should.have.property('x-rh-identity', 'identity');
-        headers.should.have.property('x-rh-insights-request-id', 'request-id');
-    });
-
-    test('obtains rule info', async function () {
-        const cache = mockCache();
-
-        const http = base.getSandbox().stub(request, 'run').resolves({
-            statusCode: 200,
-            body: {
-                count: 1,
-                page: 1,
-                per_page: 50,
-                results: [
-                    {
-                        account: '901578',
-                        bios_uuid: 'B2BC4439-2ACA-474A-8904-98F9708428AC',
-                        created: '2018-12-19T14:59:47.954014Z',
-                        display_name: null,
-                        facts: [
-                            {
-                                namespace: 'satellite',
-                                facts: {
-                                    satellite_instance_id: '3ec37799-a5b9-418f-a763-9d2e0ccf1fff'
-                                }
-                            }
-                        ],
-                        fqdn: 'packer-rhel7',
-                        id: '9615dda7-5868-4957-88ba-c3064c86d332',
-                        insights_id: '3ec37799-a5b9-418f-a763-9d2e0ccf1ff7',
-                        ip_addresses: [
-                            '10.0.2.15'
-                        ],
-                        mac_addresses: [
-                            '08:00:27:d3:0c:80',
-                            '00:00:00:00:00:00'
-                        ],
-                        rhel_machine_id: null,
-                        satellite_id: null,
-                        subscription_manager_id: 'bca8b56a-067c-4861-af4e-44add712a71e',
-                        tags: [],
-                        updated: '2018-12-19T14:59:47.954018Z'
-                    }
-                ]
-            },
-            headers: {}
+            result.should.be.empty();
+            spy.called.should.be.false();
         });
 
-        const results = await impl.getSystemDetailsBatch(['id']);
-        results.should.have.size(1);
-        results.should.have.property('9615dda7-5868-4957-88ba-c3064c86d332');
-        const result = results['9615dda7-5868-4957-88ba-c3064c86d332'];
-        result.should.have.property('id', '9615dda7-5868-4957-88ba-c3064c86d332');
-        result.should.have.property('hostname', 'packer-rhel7');
-        result.should.have.property('display_name', null);
-        result.facts[0].facts.should.have.property('satellite_instance_id', '3ec37799-a5b9-418f-a763-9d2e0ccf1fff');
+        test('forwards request headers', async function () {
+            const spy = base.getSandbox().stub(Connector.prototype, 'doHttp').resolves(inventoryResponse([]));
 
-        http.callCount.should.equal(1);
-        const options = http.args[0][0];
-        options.headers.should.have.size(2);
-        options.headers.should.have.property('x-rh-insights-request-id', 'request-id');
-        options.headers.should.have.property('x-rh-identity', 'identity');
-        cache.get.callCount.should.equal(1);
-        cache.setex.callCount.should.equal(1);
-
-        await impl.getSystemDetailsBatch(['id']);
-        cache.get.callCount.should.equal(2);
-        cache.setex.callCount.should.equal(1);
-    });
-
-    test('retries on failure', async function () {
-        const cache = mockCache();
-        const http = base.getSandbox().stub(request, 'run');
-
-        http.onFirstCall().rejects(new RequestError('Error: socket hang up'));
-        http.onSecondCall().rejects(new RequestError('Error: socket hang up'));
-        http.resolves({
-            statusCode: 200,
-            body: {
-                count: 1,
-                page: 1,
-                per_page: 50,
-                results: [
-                    {
-                        account: '901578',
-                        bios_uuid: 'B2BC4439-2ACA-474A-8904-98F9708428AC',
-                        created: '2018-12-19T14:59:47.954014Z',
-                        display_name: null,
-                        facts: [],
-                        fqdn: 'packer-rhel7',
-                        id: '9615dda7-5868-4957-88ba-c3064c86d332',
-                        insights_id: '3ec37799-a5b9-418f-a763-9d2e0ccf1ff7',
-                        ip_addresses: [
-                            '10.0.2.15'
-                        ],
-                        mac_addresses: [
-                            '08:00:27:d3:0c:80',
-                            '00:00:00:00:00:00'
-                        ],
-                        rhel_machine_id: null,
-                        satellite_id: null,
-                        subscription_manager_id: 'bca8b56a-067c-4861-af4e-44add712a71e',
-                        tags: [],
-                        updated: '2018-12-19T14:59:47.954018Z'
-                    }
-                ]
-            },
-            headers: {}
+            await impl.getSystemDetailsBatch(['id']);
+            const headers = spy.args[0][0].headers;
+            headers.should.have.size(2);
+            headers.should.have.property('x-rh-identity', 'identity');
+            headers.should.have.property('x-rh-insights-request-id', 'request-id');
         });
 
-        const results = await impl.getSystemDetailsBatch(['id']);
-        results.should.have.size(1);
-        results.should.have.property('9615dda7-5868-4957-88ba-c3064c86d332');
-        const result = results['9615dda7-5868-4957-88ba-c3064c86d332'];
-        result.should.have.property('id', '9615dda7-5868-4957-88ba-c3064c86d332');
-        result.should.have.property('hostname', 'packer-rhel7');
-        result.should.have.property('display_name', null);
+        test('obtains rule info', async function () {
+            const cache = mockCache();
 
-        http.callCount.should.equal(3);
-        const options = http.args[0][0];
-        options.headers.should.have.size(2);
-        options.headers.should.have.property('x-rh-insights-request-id', 'request-id');
-        options.headers.should.have.property('x-rh-identity', 'identity');
-        cache.get.callCount.should.equal(3);
-        cache.setex.callCount.should.equal(1);
-
-        await impl.getSystemDetailsBatch(['id']);
-        cache.get.callCount.should.equal(4);
-        cache.setex.callCount.should.equal(1);
-    });
-
-    test('returns empty object unknown systems', async function () {
-        const cache = mockCache();
-
-        const http = base.getSandbox().stub(request, 'run').resolves({
-            statusCode: 200,
-            body: {
-                count: 0,
-                page: 1,
-                per_page: 50,
-                results: [],
-                total: 0
-            },
-            headers: {}
-        });
-
-        await expect(impl.getSystemDetailsBatch(['id'])).resolves.toEqual({});
-
-        http.callCount.should.equal(1);
-        cache.get.callCount.should.equal(1);
-        cache.setex.callCount.should.equal(0);
-    });
-
-    test('handles many systems', async function () {
-        const mocks = _.keyBy(Array(250).fill(0).map((value, key) => ({
-            account: 'inventory01',
-            id: `84762eb3-0bbb-4bd8-ab11-f420c50e9${String(key).padStart(3, '0')}`,
-            insights_id: null,
-            display_name: null,
-            fqdn: `${String(key).padStart(3, '0')}.example.com`,
-            updated: '2018-12-19T14:59:47.954018Z'
-        })), 'id');
-
-        base.getSandbox().stub(request, 'run').callsFake(params => {
-            const { path } = URI.parse(params.uri);
-
-            const parts = path.split('/');
-            const ids = parts[parts.length - 1].split(',');
-            const results = _.pickBy(mocks, (value, key) => ids.includes(key));
-
-            return Promise.resolve({
+            const http = base.getSandbox().stub(request, 'run').resolves({
                 statusCode: 200,
                 body: {
-                    count: results.length,
+                    count: 1,
                     page: 1,
-                    per_page: 100,
-                    results,
-                    total: 250
+                    per_page: 50,
+                    results: [
+                        {
+                            account: '901578',
+                            bios_uuid: 'B2BC4439-2ACA-474A-8904-98F9708428AC',
+                            created: '2018-12-19T14:59:47.954014Z',
+                            display_name: null,
+                            facts: [
+                                {
+                                    namespace: 'satellite',
+                                    facts: {
+                                        satellite_instance_id: '3ec37799-a5b9-418f-a763-9d2e0ccf1fff'
+                                    }
+                                }
+                            ],
+                            fqdn: 'packer-rhel7',
+                            id: '9615dda7-5868-4957-88ba-c3064c86d332',
+                            insights_id: '3ec37799-a5b9-418f-a763-9d2e0ccf1ff7',
+                            ip_addresses: [
+                                '10.0.2.15'
+                            ],
+                            mac_addresses: [
+                                '08:00:27:d3:0c:80',
+                                '00:00:00:00:00:00'
+                            ],
+                            rhel_machine_id: null,
+                            satellite_id: null,
+                            subscription_manager_id: 'bca8b56a-067c-4861-af4e-44add712a71e',
+                            tags: [],
+                            updated: '2018-12-19T14:59:47.954018Z'
+                        }
+                    ]
                 },
                 headers: {}
             });
+
+            const results = await impl.getSystemDetailsBatch(['id']);
+            results.should.have.size(1);
+            results.should.have.property('9615dda7-5868-4957-88ba-c3064c86d332');
+            const result = results['9615dda7-5868-4957-88ba-c3064c86d332'];
+            result.should.have.property('id', '9615dda7-5868-4957-88ba-c3064c86d332');
+            result.should.have.property('hostname', 'packer-rhel7');
+            result.should.have.property('display_name', null);
+            result.facts[0].facts.should.have.property('satellite_instance_id', '3ec37799-a5b9-418f-a763-9d2e0ccf1fff');
+
+            http.callCount.should.equal(1);
+            const options = http.args[0][0];
+            options.headers.should.have.size(2);
+            options.headers.should.have.property('x-rh-insights-request-id', 'request-id');
+            options.headers.should.have.property('x-rh-identity', 'identity');
+            cache.get.callCount.should.equal(1);
+            cache.setex.callCount.should.equal(1);
+
+            await impl.getSystemDetailsBatch(['id']);
+            cache.get.callCount.should.equal(2);
+            cache.setex.callCount.should.equal(1);
         });
 
-        const ids = Array(250).fill(0).map((value, key) => `84762eb3-0bbb-4bd8-ab11-f420c50e9${String(key).padStart(3, '0')}`);
+        test('retries on failure', async function () {
+            const cache = mockCache();
+            const http = base.getSandbox().stub(request, 'run');
 
-        const result = await impl.getSystemDetailsBatch(ids);
-        result.should.have.size(250);
-        ids.forEach(id => _.has(result, id).should.be.true());
+            http.onFirstCall().rejects(new RequestError('Error: socket hang up'));
+            http.onSecondCall().rejects(new RequestError('Error: socket hang up'));
+            http.resolves({
+                statusCode: 200,
+                body: {
+                    count: 1,
+                    page: 1,
+                    per_page: 50,
+                    results: [
+                        {
+                            account: '901578',
+                            bios_uuid: 'B2BC4439-2ACA-474A-8904-98F9708428AC',
+                            created: '2018-12-19T14:59:47.954014Z',
+                            display_name: null,
+                            facts: [],
+                            fqdn: 'packer-rhel7',
+                            id: '9615dda7-5868-4957-88ba-c3064c86d332',
+                            insights_id: '3ec37799-a5b9-418f-a763-9d2e0ccf1ff7',
+                            ip_addresses: [
+                                '10.0.2.15'
+                            ],
+                            mac_addresses: [
+                                '08:00:27:d3:0c:80',
+                                '00:00:00:00:00:00'
+                            ],
+                            rhel_machine_id: null,
+                            satellite_id: null,
+                            subscription_manager_id: 'bca8b56a-067c-4861-af4e-44add712a71e',
+                            tags: [],
+                            updated: '2018-12-19T14:59:47.954018Z'
+                        }
+                    ]
+                },
+                headers: {}
+            });
+
+            const results = await impl.getSystemDetailsBatch(['id']);
+            results.should.have.size(1);
+            results.should.have.property('9615dda7-5868-4957-88ba-c3064c86d332');
+            const result = results['9615dda7-5868-4957-88ba-c3064c86d332'];
+            result.should.have.property('id', '9615dda7-5868-4957-88ba-c3064c86d332');
+            result.should.have.property('hostname', 'packer-rhel7');
+            result.should.have.property('display_name', null);
+
+            http.callCount.should.equal(3);
+            const options = http.args[0][0];
+            options.headers.should.have.size(2);
+            options.headers.should.have.property('x-rh-insights-request-id', 'request-id');
+            options.headers.should.have.property('x-rh-identity', 'identity');
+            cache.get.callCount.should.equal(3);
+            cache.setex.callCount.should.equal(1);
+
+            await impl.getSystemDetailsBatch(['id']);
+            cache.get.callCount.should.equal(4);
+            cache.setex.callCount.should.equal(1);
+        });
+
+        test('returns empty object unknown systems', async function () {
+            const cache = mockCache();
+
+            const http = base.getSandbox().stub(request, 'run').resolves({
+                statusCode: 200,
+                body: {
+                    count: 0,
+                    page: 1,
+                    per_page: 50,
+                    results: [],
+                    total: 0
+                },
+                headers: {}
+            });
+
+            await expect(impl.getSystemDetailsBatch(['id'])).resolves.toEqual({});
+
+            http.callCount.should.equal(1);
+            cache.get.callCount.should.equal(1);
+            cache.setex.callCount.should.equal(0);
+        });
+
+        test('handles many systems', async function () {
+            const mocks = _.keyBy(Array(250).fill(0).map((value, key) => ({
+                account: 'inventory01',
+                id: `84762eb3-0bbb-4bd8-ab11-f420c50e9${String(key).padStart(3, '0')}`,
+                insights_id: null,
+                display_name: null,
+                fqdn: `${String(key).padStart(3, '0')}.example.com`,
+                updated: '2018-12-19T14:59:47.954018Z'
+            })), 'id');
+
+            base.getSandbox().stub(request, 'run').callsFake(params => {
+                const { path } = URI.parse(params.uri);
+
+                const parts = path.split('/');
+                const ids = parts[parts.length - 1].split(',');
+                const results = _.pickBy(mocks, (value, key) => ids.includes(key));
+
+                return Promise.resolve({
+                    statusCode: 200,
+                    body: {
+                        count: results.length,
+                        page: 1,
+                        per_page: 100,
+                        results,
+                        total: 250
+                    },
+                    headers: {}
+                });
+            });
+
+            const ids = Array(250).fill(0).map((value, key) => `84762eb3-0bbb-4bd8-ab11-f420c50e9${String(key).padStart(3, '0')}`);
+
+            const result = await impl.getSystemDetailsBatch(ids);
+            result.should.have.size(250);
+            ids.forEach(id => _.has(result, id).should.be.true());
+        });
     });
 
     describe('getSystemsProfileBatch', function () {
