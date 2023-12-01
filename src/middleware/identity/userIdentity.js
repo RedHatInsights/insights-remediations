a8a499e9b4fe8215d2b9eb4db700d0829812d102
Rex White <rexwhite@redhat.com>
2023-12-01 18:41:43 -0500

    RHINENG-5482 - Added support for service accounts (#405)

--- src/middleware/identity/userIdentity.js
+++ src/middleware/identity/userIdentity.js
@@ -3,9 +3,13 @@
 const errors = require('../../errors');
 
 module.exports = function (req, res, next) {
-    if (req.identity.type !== 'User' || !req.identity.user.username || req.identity.user.is_internal === undefined) {
-        return next(new errors.Forbidden());
+    if (req.identity.type !== 'User' && req.identity.type !== 'ServiceAccount') {
+        return next(new errors.Forbidden('Supplied identity not valid for requested operation'));
     }
 
-    next();
+    if (!req.identity.user.username || req.identity.user.is_internal === undefined) {
+        return next(new errors.Forbidden('Supplied identity invalid'));
+    }
+
+    return next();
 };
