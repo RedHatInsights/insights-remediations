beaa83c6ef9e3829da84c822f1ba5cfe3c4c3527
Rex White <rexwhite@redhat.com>
2024-01-08 17:14:50 -0500

    RHINENG-5482: fixed silly logic error

--- src/middleware/identity/userIdentity.js
+++ src/middleware/identity/userIdentity.js
@@ -1,18 +1,17 @@
 'use strict';
 
 const errors = require('../../errors');
-const _ = require('lodash');
 
 module.exports = function (req, res, next) {
-    switch (req.identity.type) {
+    switch(req.identity.type) {
         case 'User':
-            if ( !_.get(req, 'identity.user.username') || !_.get(req, 'identity.user.is_internal') ) {
-                return next(new errors.Forbidden('Supplied user identity invalid'));
+            if (!req.identity?.user?.username || req.identity?.user?.is_internal === undefined) {
+                return next(new errors.Forbidden('Supplied identity invalid'));
             }
             break;
 
         case 'ServiceAccount':
-            if (!_.get(req, 'identity.service_account.username')) {
+            if (!req.identity?.service_account?.username) {
                 return next(new errors.Forbidden('Supplied service account identity invalid'));
             }
             break;
