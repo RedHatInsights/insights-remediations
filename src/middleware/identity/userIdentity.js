4284bfe1750ab33c3228ed576b63f3c2c03a48fc
Rex White <rexwhite@redhat.com>
2024-01-08 17:23:40 -0500

    RHINENG-5482 Fixed error in identity header handling (#406)
    * RHINENG-5482 Fixed error in identity header handling
    
    * RHINENG-5482: fixed silly logic error

--- src/middleware/identity/userIdentity.js
+++ src/middleware/identity/userIdentity.js
@@ -3,12 +3,21 @@
 const errors = require('../../errors');
 
 module.exports = function (req, res, next) {
-    if (req.identity.type !== 'User' && req.identity.type !== 'ServiceAccount') {
-        return next(new errors.Forbidden('Supplied identity not valid for requested operation'));
-    }
+    switch(req.identity.type) {
+        case 'User':
+            if (!req.identity?.user?.username || req.identity?.user?.is_internal === undefined) {
+                return next(new errors.Forbidden('Supplied identity invalid'));
+            }
+            break;
 
-    if (!req.identity.user.username || req.identity.user.is_internal === undefined) {
-        return next(new errors.Forbidden('Supplied identity invalid'));
+        case 'ServiceAccount':
+            if (!req.identity?.service_account?.username) {
+                return next(new errors.Forbidden('Supplied service account identity invalid'));
+            }
+            break;
+
+        default:
+            return next(new errors.Forbidden('Supplied identity not valid for requested operation'));
     }
 
     return next();
