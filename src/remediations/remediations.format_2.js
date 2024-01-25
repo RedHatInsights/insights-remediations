b71ca41742dd48ce8531e03c348132d5d8c8d3a1
Rex White <rexwhite@redhat.com>
2024-01-25 14:45:42 -0500

    RHINENG-7766: Added system_id to rhc-direct work request for playbook dispatcher (#420)

--- src/remediations/remediations.format_2.js
+++ src/remediations/remediations.format_2.js
@@ -83,19 +83,41 @@ exports.connectionStatus = function (recipients) {
 };
 
 
+//------------------------------------------------------------------------------------------------------------
+// rhcDirectWorkRequestV2 - returns json suitable for submission to playbook-dispatcher/internal/v2/dispatch
+//
+//   recipient:
+//   [
+//       {
+//           "org_id": "5318290",
+//           "recipient": "32af5948-301f-449a-a25b-ff34c83264a2",
+//           "recipient_type": "directConnect",
+//           "sat_id": "",
+//           "sat_org_id": "",
+//           "status": "connected",
+//           "systems": [
+//               "fe30b997-c15a-44a9-89df-c236c3b5c540"
+//           ]
+//       }
+//   ]
 
-exports.rhcDirectWorkRequestV2 = function (playbookRunId, target, remediation, username) {
+exports.rhcDirectWorkRequestV2 = function (playbookRunId, recipient, remediation, username) {
     const work_request =  {
-        recipient: target.recipient,
-        org_id: target.org_id,
+        recipient: recipient.recipient,
+        org_id: recipient.org_id,
         url: new URI(`https://${config.platformHostname}`)
             .segment([config.path.prefix, config.path.app, `v1/remediations/${remediation.id}/playbook`])
-            .search({hosts: target.systems, localhost: null}) 
+            .search({hosts: recipient.systems, localhost: null})
             .toString(),
         name: remediation.name,
         principal: username,
         web_console_url: "https://console.redhat.com/insights/remediations",
-        labels: { 'playbook-run': playbookRunId }
+        labels: { 'playbook-run': playbookRunId },
+        hosts: [
+            {
+                inventory_id: recipient.systems[0]  // there should only be one...
+            }
+        ]
     };
 
     return work_request;
