dbdd19f506b229e35019bea11144dcac816236a2
Rex White <rexwhite@redhat.com>
2024-05-03 14:03:49 -0400

    RHINENG-9857: rename existing remediation plans with leading/trailing whitespace

--- db/migrations/20240502174415-fix-invalid-plan-names.js
+++ db/migrations/20240502174415-fix-invalid-plan-names.js
@@ -1,17 +1,38 @@
 'use strict';
 
 module.exports = {
-  up: async (q, {STRING}) => {
-    // Fix plan names of the form " - 1"
-    const fix_blank_names = "UPDATE remediations SET name = 'Blank' || name WHERE name LIKE ' -%';";
-    await q.sequelize.query(fix_blank_names);
-
-    // remove leading blanks from plan names
-    const remove_spaces = "UPDATE remediations SET name = TRIM(name) WHERE name LIKE ' %';";
-    await q.sequelize.query(remove_spaces);
-  },
-
   down: async (q, Sequelize) => {
     // there is no meaningful reset for this migration
+  },
+
+  up: async (q, {STRING}) => {
+    // Fix plan names that would be duplicates once TRIM()-ed
+    const fix_dup_names = `
+        WITH dups AS (
+            SELECT b.name, b.id 
+            FROM remediations a 
+            JOIN (
+                SELECT TRIM(name) name, id, tenant_org_id 
+                FROM remediations 
+                WHERE name LIKE ' %' OR name LIKE '% '
+            ) b 
+            ON a.name = b.name AND a.tenant_org_id = b.tenant_org_id
+        )
+        UPDATE remediations
+        SET name = dups.name || ' - 1'
+        FROM dups
+        WHERE dups.id = remediations.id;
+    `;
+
+    await q.sequelize.query(fix_dup_names);
+
+    // trim remaining plan names
+    const remove_spaces = `
+        UPDATE remediations
+        SET name = TRIM(name)
+        WHERE name LIKE ' %' OR name LIKE '% ';
+    `;
+
+    await q.sequelize.query(remove_spaces);
   }
 };
