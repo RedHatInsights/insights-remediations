baaff53828481f0924fec31a18c03ba0444512f6
Rex White <rexwhite@redhat.com>
2024-05-30 11:22:50 -0400

    RHINENG-10058: Handle name collisions better.

--- db/migrations/20240517140534-avoid-tabs-with-postgres.js
+++ db/migrations/20240517140534-avoid-tabs-with-postgres.js
@@ -6,33 +6,36 @@ const _ = require('lodash');
 //
 // Something about the combination of Sequelize and postgres makes sql containing \t's behave unpredictably,
 // so we're doing this migration again, the tedious way...
+
 exports.up = async (q) => {
-    // get list of remediation plans
-    const [plans, metadata] = await q.sequelize.query("SELECT name, tenant_org_id, id FROM remediations;");
 
-    // find plan names with leading/trailing tabs and strip them
-    const re = /^\t|\t$/;
-    const fixedPlans = _(plans)
-        .filter(plan => {
-            if (re.test(plan.name)) {
-                plan.name = plan.name.trim();
-                return true;
-            }
+    async function collides (plan_name, tenant_org_id) {
+        const dups_sql = `SELECT name FROM remediations WHERE name = '${plan_name}' AND tenant_org_id = '${tenant_org_id}'`;
+        const [dups] = await q.sequelize.query(dups_sql);
 
-            return false;
-        })
-        .value();
+        return dups.length > 0;
+    }
 
-    // find plans that are duplicates due to the name change and make them unique
-    for (const plan of fixedPlans) {
-        // check for any matches
-        if (_(plans).find({'name': plan.name}).value()) {
-            // disambiguate by appending ' - 1' to plan name
-            plan.name += ' - 1';
+    // get list of remediation plans with leading / trailing tabs
+    const target_sql = "SELECT name, tenant_org_id, id FROM remediations WHERE name like '\t%' OR name LIKE '%\t';";
+    const [target_plans] = await q.sequelize.query(target_sql);
+
+    for (const plan of target_plans) {
+        // remove leading/trailing whitespace from target plan name
+        plan.name = plan.name.trim();
+
+        // check for collisions with existing plans
+        let dup_count = 0;
+        let new_name = plan.name;
+
+        while (await collides(new_name, plan.tenant_org_id)) {
+            dup_count++;
+            new_name = plan.name + ` - ${dup_count}`;
         }
 
-        // update changed plans!
-        await q.sequelize.query(`UPDATE remediations SET name = ${plan.name} WHERE id = ${plan.id};`);
+        // update plan name
+        const update_sql = `UPDATE remediations SET name = '${new_name}' WHERE id = '${plan.id}';`;
+        await q.sequelize.query(update_sql);
     }
 };
 
