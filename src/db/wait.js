0619864602afe83258b5e71182df2bfa64c42d70
Rex White <rexwhite@redhat.com>
2024-07-18 22:42:58 -0400

    RHINENG-11505: Added npm db:wait run target to wait for db to initialize

---
+++ src/db/wait.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// This module implements a simple utility that waits for the db connection to become available
+const config = require('../config').db;
+const pg = require('pg');
+
+const RETRIES = 5;
+const DELAY = 3000;
+const SUCCESS = 0;
+const FAILURE = -1;
+
+const pg_config = {
+    user: config.username,
+    password: config.password,
+    host: config.host,
+    port: config.port,
+    database: config.database
+};
+
+function wait (retries) {
+    if (retries > 0) {
+        const client = new pg.Client(pg_config);
+
+        client.connect()
+        .then(() => {
+            client.query("SELECT 'DB is alive!' AS result;")
+            .then((res) => {
+                console.log(`${JSON.stringify(res?.rows)}`);
+            })
+            .then(() => {
+                client.end()
+                // we connected - return success!
+                .then(() => {process.exit(SUCCESS);});
+            });
+        })
+
+        .catch((error) => {
+            // retry connection...
+            setTimeout(() => {wait(retries - 1);}, DELAY);
+        });
+    }
+
+    else {
+        // failed to connect!
+        process.exit(FAILURE);
+    }
+}
+
+module.exports = { wait };
+
+if (require.main === module) {
+    wait(RETRIES);
+}
