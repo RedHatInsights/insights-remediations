ebb30c5fc90d0d0cc0b67bbc6e92b059f6412e29
Rex White <rexwhite@redhat.com>
2024-07-23 16:56:53 -0400

    RHINENG-11246: enable tracing for playbook download endpoint (#516)
    * RHINENG-11246: enable tracing for playbook download endpoint
    
    * Fixed typo in trace path

--- src/remediations/playbook/routes.js
+++ src/remediations/playbook/routes.js
@@ -3,11 +3,12 @@
 const read = require('../controller.read');
 const openapi = require('../../middleware/openapi');
 const rbac = require('../../middleware/rbac');
+const trace = require("../../util/trace").middleware;
 
 const rbacRead = rbac('remediations:remediation:read');
 
 module.exports = function (router) {
     router.route('/remediations/:id/playbook')
-        .get(openapi('getRemediationPlaybook'), rbacRead, read.playbook)
+        .get(trace, openapi('getRemediationPlaybook'), rbacRead, read.playbook)
         .post(openapi('bulkGetRemediationPlaybook'), rbacRead, read.bulkPlaybook);
 };
