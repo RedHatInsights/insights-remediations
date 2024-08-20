baa9f0ab7a34f80be7cfeda268e8883b4520d63a
Rex White <rexwhite@redhat.com>
2024-08-20 10:42:03 -0400

    RHINENG-11246: Fixed order of columns in table index

--- db/migrations/20240820140920-add-index-on-issue-resolution.js
+++ db/migrations/20240820140920-add-index-on-issue-resolution.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   async up (q) {
-    q.addIndex('remediation_issue_systems', ['resolved', 'remediation_issue_id']);
+    q.addIndex('remediation_issue_systems', ['remediation_issue_id', 'resolved']);
   },
 
   async down (q) {
-    q.removeIndex('remediation_issue_systems', ['resolved', 'remediation_issue_id']);
+    q.removeIndex('remediation_issue_systems', ['remediation_issue_id', 'resolved']);
   }
 };
