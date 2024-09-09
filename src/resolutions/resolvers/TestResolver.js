149ac9460613e98364c51ad3029e6b78afa02f07
Marley Stipich <87209745+marleystipich2@users.noreply.github.com>
2024-09-09 12:52:55 -0400

    RHINENG-12382: Add test issues that generate large playbook outputs

--- src/resolutions/resolvers/TestResolver.js
+++ src/resolutions/resolvers/TestResolver.js
@@ -46,7 +46,10 @@ const RESOLUTIONS = Object.freeze({
         templates.test.invalidSignatureVars,
         'fix',
         'Template with invalid signature due to incorrect vars field'
-    )]
+    )],
+    largePlaybook1mb: [new Resolution(templates.test.largePlaybook1mb, 'fix', 'Run Ansible large playbook module with 1 MB output')],
+    largePlaybook2mb: [new Resolution(templates.test.largePlaybook2mb, 'fix', 'Run Ansible large playbook module with 2 MB output')],
+    largePlaybook5mb: [new Resolution(templates.test.largePlaybook5mb, 'fix', 'Run Ansible large playbook module with 5 MB output')],
 });
 
 module.exports = class TestResolver extends Resolver {
