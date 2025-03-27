dada104f5a8261dafe6eae3e5f61021ff5f4db36
Rex White <rexwhite@redhat.com>
2025-03-27 16:19:00 -0400

    Revert "RHINENG-17071: Adjust code to use Compliance API v2 endpoint" (#613)
    This reverts commit bdfb95beff9ddb20a4fe542167b870eff5de5482.

--- src/util/identifiers.js
+++ src/util/identifiers.js
@@ -1,23 +1,8 @@
 'use strict';
 
 const errors = require('../errors');
-/*
-Examples of possible identifiers: 
-Advisor:
-    - advisor:CVE_2017_6074_kernel|KERNEL_CVE_2017_6074
-Vulnerability:
-    - vulnerabilities:CVE-2017-17712
-Compliance:
-    - ssg:rhel7|standard|xccdf_org.ssgproject.content_rule_service_autofs_disabled
-    - ssg:rhel7|0021d5a8-6573-4766-8bfd-5f5eab59015c|pci-dss|xccdf_org.ssgproject.content_rule_disable_prelink
-Test:
-    - test:ping
-Patch:
-    - patch-advisory:RHBA-2019:4105
-    - patch-package:rpm-4.14.2-37.el8.x86_64
-*/
 const PATTERN = /^(advisor|vulnerabilities|ssg|test|patch-advisory|patch-package):([\w\d_|:\\.+-]+)$/;
-const SSG_PATTERN = /^([\w-]+)(\|[\w-]+)?\|([\w-]+)\|xccdf_org\.ssgproject\.content_rule_([\w\d-_:\\.]+)$/;
+const SSG_PATTERN = /^([\w-]+)\|([\w-]+)\|xccdf_org\.ssgproject\.content_rule_([\w\d-_:\\.]+)$/;
 const CSAW_PATTERN = /^(CVE-20[\d]{2}-[\d]{4,}):(\w+\|[A-Z\d_]+)$/;
 const CSAW_RULE_PATTERN = /^(\w+\|[A-Z\d_]+)$/;
 
@@ -79,18 +64,15 @@ exports.parseSSG = function (id) {
 
     const result = SSG_PATTERN.exec(id.issue);
 
-    if (!result || result.length !== 5) {
+    if (!result || result.length !== 4) {
         throw errors.invalidIssueId(id);
     }
 
-    // result[2] will be undefined if there is no security guide id in the issue id
-    // so set securityGuideId to null in this case
     return {
         platform: result[1],
-        securityGuideId: result[2] || null,
-        profile: result[3],
-        rule: result[4],
-        ruleRef: `xccdf_org.ssgproject.content_rule_${result[4]}`
+        profile: result[2],
+        rule: result[3],
+        ruleRef: `xccdf_org.ssgproject.content_rule_${result[3]}`
     };
 };
 
