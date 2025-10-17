18550a98ab94a040bc5657e54a0568ae0824e4db
Richard Brantley <brantleyr@gmail.com>
2025-10-17 12:37:29 -0400

    remove smart_management - no longer used

--- src/middleware/identity/utils.js
+++ src/middleware/identity/utils.js
@@ -99,9 +99,6 @@ exports.createServiceAccountIdentityHeader = function (
             openshift: {
                 is_entitled: true
             },
-            smart_management: {
-                is_entitled: true
-            },
             hybrid_cloud: {
                 is_entitled: true
             }
