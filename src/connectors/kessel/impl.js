ea2b861ad27fe571b645b6b57514b5194a1f3995
Richard Brantley <brantleyr@gmail.com>
2025-11-05 11:38:37 -0500

    semicolon

--- src/connectors/kessel/impl.js
+++ src/connectors/kessel/impl.js
@@ -7,7 +7,7 @@ const log = require('../../util/log');
 const metrics = require('../metrics');
 
 // Import new Kessel SDK with ClientBuilder
-let ClientBuilder, fetchOIDCDiscovery, OAuth2ClientCredentials, OAuth2AuthRequest, fetchDefaultWorkspace, Allowed
+let ClientBuilder, fetchOIDCDiscovery, OAuth2ClientCredentials, OAuth2AuthRequest, fetchDefaultWorkspace, Allowed;
 try {
     // Import the new ClientBuilder from the updated SDK
     const kesselSdk = require('@project-kessel/kessel-sdk/kessel/inventory/v1beta2');
