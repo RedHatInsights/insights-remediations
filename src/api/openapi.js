5a6edb0465b69cdc13410f710f488b96dc375220
Marley Stipich <mstipich@redhat.com>
2025-11-18 13:47:40 -0500

    Change yaml.safeLoad to yaml.load

--- src/api/openapi.js
+++ src/api/openapi.js
@@ -9,7 +9,7 @@ const OpenAPISchemaValidator = require('openapi-schema-validator').default;
 
 const specPath = path.join(__dirname, './openapi.yaml');
 // eslint-disable-next-line security/detect-non-literal-fs-filename
-const spec = jsyaml.safeLoad(fs.readFileSync(specPath, 'utf8'));
+const spec = jsyaml.load(fs.readFileSync(specPath, 'utf8'));
 
 const validator = new OpenAPISchemaValidator({ version: 3 });
 const result = validator.validate(spec);
