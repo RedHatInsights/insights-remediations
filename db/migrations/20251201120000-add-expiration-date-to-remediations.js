5ece206a8d4ed9a2a7808950e3bb88a8dba71ca1
Richard Brantley <brantleyr@gmail.com>
2026-02-10 14:44:15 -0500

    Add index for new expiration_date column

--- db/migrations/20251201120000-add-expiration-date-to-remediations.js
+++ db/migrations/20251201120000-add-expiration-date-to-remediations.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const EXPIRATION_DATE_INDEX = 'remediations_expiration_date_idx';
+
 module.exports = {
   up: async (q, {DATE}) => {
     await q.addColumn('remediations', 'expiration_date', {
@@ -7,9 +9,13 @@ module.exports = {
       allowNull: true,
       defaultValue: null
     });
+    await q.addIndex('remediations', ['expiration_date'], {
+      name: EXPIRATION_DATE_INDEX
+    });
   },
 
   down: async (q) => {
+    await q.removeIndex('remediations', EXPIRATION_DATE_INDEX);
     await q.removeColumn('remediations', 'expiration_date');
   }
 };
