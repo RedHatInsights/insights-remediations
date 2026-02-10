32c161432f7e0ad48d9c647828d749e2f3b1c11c
Richard Brantley <brantleyr@gmail.com>
2026-02-10 12:39:28 -0500

    Add index for new column

--- db/migrations/20250210120000-add-workspace-id-to-remediations.js
+++ db/migrations/20250210120000-add-workspace-id-to-remediations.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const WORKSPACE_ID_INDEX = 'remediations_workspace_id_idx';
+
 module.exports = {
   up: async (q, {STRING}) => {
     await q.addColumn('remediations', 'workspace_id', {
@@ -7,9 +9,13 @@ module.exports = {
       allowNull: true,
       defaultValue: null
     });
+    await q.addIndex('remediations', ['workspace_id'], {
+      name: WORKSPACE_ID_INDEX
+    });
   },
 
   down: async (q) => {
+    await q.removeIndex('remediations', WORKSPACE_ID_INDEX);
     await q.removeColumn('remediations', 'workspace_id');
   }
 };
