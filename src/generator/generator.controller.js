552929e9df4ff3260141436c5df6f0c34ecf8622
Marley Stipich <mstipich@redhat.com>
2026-04-07 08:25:48 -0400

    RHINENG-24044: Add optional systemIds to failedGeneration probe function

--- src/generator/generator.controller.js
+++ src/generator/generator.controller.js
@@ -47,7 +47,7 @@ exports.playbookPipeline = async function ({issues, auto_reboot = true}, remedia
 
         // Always throw an error if an issue cannot be added to the playbook
         // to prevent generating partial playbooks with missing issues
-        probes.failedGeneration();
+        probes.failedGeneration({ issueId: issue.id });
         throw e;
     })).filter(issue => issue);
 
@@ -153,7 +153,7 @@ exports.resolveSystems = async function (issues, strict = true) {
     // - strict=false: return partial results with only known systems
     const systems = await inventory.getSystemDetailsBatch(systemIds, true, 2, strict)
         .catch(e => {
-            probes.failedGeneration();
+            probes.failedGeneration({ systemIds });
             throw e;
         });
 
