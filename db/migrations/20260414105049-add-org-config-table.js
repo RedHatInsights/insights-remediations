4b5c4b6924cc7268d7f210d546f7643180f28536
Marley Stipich <mstipich@redhat.com>
2026-04-14 13:12:35 -0400

    RHINENG-25792: Create org_config DB table and model

---
+++ db/migrations/20260414105049-add-org-config-table.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+    async up (q, {DATE, INTEGER, TEXT, fn}) {
+        await q.createTable('org_config', {
+            org_id: {
+                type: TEXT,
+                primaryKey: true,
+                allowNull: false,
+                comment: 'Organization ID'
+            },
+            retention_period_days: {
+                type: INTEGER,
+                allowNull: false,
+                defaultValue: 120,
+                comment: 'Days of inactivity before remediation plan expiration'
+            },
+            retention_warning_days: {
+                type: INTEGER,
+                allowNull: false,
+                defaultValue: 30,
+                comment: 'Days before expiration to show warning'
+            },
+            created_at: {
+                type: DATE,
+                allowNull: false,
+                defaultValue: fn('now')
+            },
+            updated_at: {
+                type: DATE,
+                allowNull: false,
+                defaultValue: fn('now')
+            }
+        });
+    },
+
+    async down (q) {
+        await q.dropTable('org_config');
+    }
+};
